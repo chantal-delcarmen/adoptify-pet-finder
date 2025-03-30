12489b04b404aea2dcd8ef346ae481f380749f73
Chantal <104171352+RNBVE@users.noreply.github.com>
2025-03-29 23:32:46 -0600

    Refactor settings.py to use environment variables for SECRET_KEY and DEBUG settings

--- backend/adoptify_backend/settings.py
+++ backend/adoptify_backend/settings.py
@@ -21,11 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-a+@zkojsqn9b-z(m=lu$di=o&&=@ns884eq=$-udigdon6v&u('
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
 DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
