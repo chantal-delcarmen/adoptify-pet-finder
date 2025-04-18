27ea1f287191a80286f517f307e25073738ea9ee
MohammadSoomro <mohammad.soomro@ucalgary.ca>
2025-04-18 15:43:50 -0600

    new commit

--- backend/adoptify_backend/urls.py
+++ backend/adoptify_backend/urls.py
@@ -32,4 +32,5 @@ urlpatterns = [
     path("api/token/refresh", TokenRefreshView.as_view(), name="refresh"),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include("api.urls")),
+    path("api/favourites/", views.AddFavouriteView.as_view(), name="add_favourite"),
 ]
